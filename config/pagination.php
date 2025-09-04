6ce69056bf79d5430f1b780326fe52426ffb0309
tetsuyaEbina <tetsuya.ebina26@gmail.com>
2025-09-04 21:07:09 +0900

    アプリケーションのtimezoneを修正

---
+++ config/pagination.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Default Count
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the default number of items shown per page when
+    | using pagination. You can override it on a per-query basis if needed.
+    |
+    */
+
+    'default'        => 20,
+    'user_companies' => 40,
+];
